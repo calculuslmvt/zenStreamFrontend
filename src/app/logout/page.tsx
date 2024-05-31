7b8af3ff0d0355deda1f87580d798fbc147fb0c9
Calculuslmvt <calculuslmvt@gmail.com>
2024-05-31 21:39:15 +0530

    Fixed deployement issue, Window and Location

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,15 +1,18 @@
 "use client";
 import { useUserStore } from '@/store/store';
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function Logout() {
 
   const updateUsername = useUserStore((state) => state.updateUsername);
   updateUsername(""); 
-  if(typeof window !== undefined) {
-    window.sessionStorage.setItem("username", "");
-  }
-  location.replace("/");
+  useEffect(()=>{
+      if(typeof window !== undefined) {
+      window.sessionStorage.setItem("username", "");
+      location.replace("/");
+    }
+  })
+
   return (
     <div>
         Logout success !!
